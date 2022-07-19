3fa11d15c7fa5c64f6cc4cda839e103f08921298
Chemi Martinez <jmmartineztuarez@gmail.com>
2022-07-18 21:42:51 -0400

    Update urls.py

--- workbook/core/urls.py
+++ workbook/core/urls.py
@@ -15,21 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf import settings
+from . import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-
-    urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('security/',include("workbook.modules.security.urls")),
-        path('',include("workbook.modules.main.urls")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
-else:
-    urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('security/',include("workbook.modules.security.urls")),
-        path('',include("workbook.modules.main.urls")),
-    ]
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('security/',include("workbook.modules.security.urls")),
+    path('',include("workbook.modules.main.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # type: ignore
 
     
