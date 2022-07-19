4185ec589aaaaed98288f7d32e8d757575b50224
Chemi Martinez <jmmartineztuarez@gmail.com>
2022-07-18 21:23:01 -0400

    modules fix

--- workbook/modules/main/apps.py
+++ workbook/modules/main/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class MainConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'modules.main'
+    name = 'workbook.modules.main'
